54d7920d61382e68506f2df343b9342bba932f33
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2024-05-27 18:15:31 +0530

    basic part done

---
+++ Javascript/Variable/day1.js
@@ -0,0 +1,32 @@
+console.log('Welcome to javascript');
+
+//console is the predefined object
+//log is the function
+
+console.log(10+'10'); //concatination
+
+console.log(10+ +"10");
+
+console.log(10+"10"+10); //concatination
+
+console.log(100/'0');
+
+console.log(100*100);
+console.log(10>9>8);  //1>8 (false)
+
+console.log(1+ +true); //true
+
+//Variable
+//it is used to store the data 
+//it holds Particular memory location
+//Any type of data we can store string,number,boolean,object,array...
+
+//how to declare variable
+//var let const
+
+//let and const introduced in Es6 version
+
+
+
+
+
