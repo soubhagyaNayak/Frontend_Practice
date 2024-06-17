ed8cb7875757ff8494aa3a73be2c123126fc370a
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2024-06-17 10:41:55 +0530

    array

---
+++ Array/day1.js
@@ -0,0 +1,40 @@
+//Find the length of the array
+
+let arr1=[10,20,30,40,50,60];
+//console.log(arr1.length);
+
+//push function used with array for adding an element in the end
+
+//arr1.push(70);
+//console.log(arr1);
+
+/*pop function is used with array for removing an element from the end
+arr1.pop();
+console.log(arr1);
+*/
+//how can we reduce an array length
+// arr1.length=4;
+// console.log(arr1);
+
+// delete arr1[3];
+// console.log(arr1);
+// console.log(arr1.length);
+
+//adding the element from the beginning 
+// arr1.unshift(5);
+//removing the element from the beginning
+// arr1.shift();
+// console.log(arr1);
+
+// let arr2=[10,20,30,40,50,60];
+// let arr3=[70,80,90];
+//using the spread operator we can merge two array
+// let arr4=[...arr2,...arr3];
+// console.log(arr4);
+
+//using spread operator how can we copy an array
+// let arr5=[1,2,3];
+// let arr6=[...arr5];
+// console.log(arr6);
+
+
